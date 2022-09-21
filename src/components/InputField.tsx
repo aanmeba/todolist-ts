061db3b373c635fab35b1aba725cb0f37c2c4b40
aanmeba <aanmeba@gmail.com>
2022-09-21 15:21:14 +1000

    setup project

---
+++ src/components/InputField.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InputField = () => {
+  return (
+    <form className="form">
+      <input type="input" className="input_field" />
+      <button className="input_submit">Go</button>
+    </form>
+  );
+};
+
+export default InputField;
