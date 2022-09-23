cdf1206ed886d251c2cacb0a1d7f45f83a68067a
aanmeba <aanmeba@gmail.com>
2022-09-23 19:06:29 +1000

    edit & delete functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
 import { Todo } from "../model";
@@ -10,9 +10,11 @@ interface Props {
 }
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
-  const handleDone = (id: number) => {
-    console.log(`done ${id}`);
+  const [edit, setEdit] = useState(todo.id);
+  const [isEditClicked, setIsEditClicked] = useState(false);
+  const inputField = useRef(null);
 
+  const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
         // todo.id === id ? (todo.isDone = !todo.isDone) : todo
@@ -21,17 +23,44 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     );
   };
 
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (id: number) => {
+    console.log(todo);
+    setIsEditClicked(!isEditClicked);
+    setEdit(id);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+
+    setTodos(
+      todos.map((todo) =>
+        todo.id === edit ? { ...todo, todo: e.target.value } : todo
+      )
+    );
+  };
+
   return (
     <div key={todo.id}>
       {todo.isDone ? (
         <s className="todolist_text">{todo.todo} </s>
+      ) : isEditClicked ? (
+        <input
+          type="text"
+          value={todo.todo}
+          ref={inputField}
+          onChange={handleChange}
+        />
       ) : (
-        <span className="todolist_text">{todo.todo} </span>
+        <span className="todolist_text">{todo.todo}</span>
       )}
-      <span className="icons">
+      <span className="icons" onClick={() => handleEdit(todo.id)}>
         <AiOutlineEdit />
       </span>
-      <span className="icons">
+      <span className="icons" onClick={() => handleDelete(todo.id)}>
         <AiOutlineDelete />
       </span>
       <span className="icons" onClick={() => handleDone(todo.id)}>
